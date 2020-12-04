b27807f4df555e3461fc7388b43458e057b06bfa
alexeymaranin <alexeymaranin@gmai.com>
2020-12-04 17:53:26 +0300

    added course page

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ $(function() {
     let scrollPos = $(window).scrollTop();
     let nav = $("#nav");
     let navToggle = $("#navToggle");
-
+    let description = $("#description")
+    let content = $("#content")
+    let contentContent = $("#contentContent")
+    let descriptionContent = $("#descriptionContent")
 
 
 
@@ -21,6 +24,27 @@ $(function() {
 
         nav.toggleClass("show");
     });
+
+    content.on("click", function(event) {
+        event.preventDefault();
+
+        descriptionContent.addClass("hide");
+        contentContent.addClass("show");
+        content.addClass("extra__active");
+        description.removeClass("extra__active");
+
+
+    });
+
+    description.on("click", function(event) {
+        event.preventDefault();
+
+        contentContent.removeClass("show");
+        descriptionContent.removeClass("hide");
+        content.removeClass("extra__active");
+        description.addClass("extra__active");
+    });
+
 
 
 })
